6885fbcf006e3269ce6ec6a9b9c33562573238b0
Breanna Sand <breanna.sand8@gmail.com>
2023-12-17 11:31:53 +0100

    active class css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!!</div>;
+  return (
+    //wrap app with BrowserRouter so that all components within can use links for navigating between pages
+    <BrowserRouter>
+      <main>
+        <div>Find me in src/app.jsx!!</div>;
+
+
+      </main>
+
+    </BrowserRouter>
+  )
 };
